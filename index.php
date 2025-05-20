d332fd01b798594e6d347419321b5e67e275ed17
Joshua <105934191@student.swin.edu.au>
2025-05-20 16:18:15 +1000

    i have updated the html documents pages 1 to 4 to be a php file and replaced the headers with each of them, with the inc file header and footer

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang = "en">
+  <head>
+      <!--Title of the web page-->
+      <title>JigSaw</title>
+      <meta charset ="UTF-8">
+      <meta name="description" content="menu of JigSaw">
+      <!--Keywords-->
+      <meta name="keywords" content="Description of company, title, logo">
+      <!--Author information-->
+      <meta name="author" content="Teagan O'Shannassy">
+       <!--External CSS files-->
+      <link rel="stylesheet" type="text/css" href="styles/styles.css">
+          <!-- Embedded CSS -->
+  </head>
+    <!--This part of the code was taken in reference from Ati's Github-->
+  <body id="content">
+    <!--replaced the header with a php include-->
+    <?php include 'header.inc'; ?>
+    </section>
+       <section id="brief">
+           <div id="info" class="BriefText">
+               <h1>Protecting and Improving Cyber Resilience each and every day</h1>
+               <p>Detecting and neutralising threats online with our superior security and amazing service.</p>
+           </div>
+      </section>
+      <!--reference from w3school on both html and css for formating and classes-->
+       <section id="table">
+           <div class="side">
+               <!--Information about servies avaliable-->
+               <h2>Managed Security Services</h2>
+               <p>Come and Work closely with JigSaw cyber experts to enhance your security and accelerate your softwares resilience
+                   to cyber attacks.</p>
+           </div>
+           <div class="about">
+               <!--About the company-->
+               <h2>About Us</h2>
+               <p>We are a manager cybersecurity company, JigSaw based in Victoria, VIC. We speicialise in developing and
+                   immplementing secure software and hard ware solutions forbuisnesses of every category. We are a team of
+                   passionate and engaging cyber engineers and designers that are dedicated to provide and devliver reliable
+                   and high quality products for their clients.</p>
+           </div>
+           <div class="side">
+               <h2>Pioneering Cyber Innovation</h2>
+               <p>Jigsaw is considered a leading user and data security cybersecurity company, entrusted to safeguard both organizations
+                   and individuals while driving digital transformation and growth. Our range of solutions are adapted in real-time to how
+                   people interact with data, providing secure access while creating new value.</p>
+           </div>
+       </section>
+      <!--Short description about avaliable jobs-->
+      <section class="avaliable">
+      <h2>Who are we looking for?</h2>
+      <p>Individuals who have experience in a competitive workspace and have the following requirements: </p>
+
+      <!--Unordered List for requirements-->
+          <ul>
+           <li><strong>Experience:</strong> Has some form of work experience in competitive work space</li>
+           <li><strong>Education</strong> Has completed a higher education, such as masters, diploma or bachelor</li>
+           <li><strong>Collaboration</strong> Able to work with other individuals in a group setting</li>
+           <li><strong>Technical Skills</strong> Proficiency in scripting languages like Python, C or other languages.</li>
+          </ul>
+          <p>And more depending on job position and urgency to fill positions.</p>
+       <h2>Interested in applying?</h2>
+       <p>If you are interested in applying for a potential position at JigSaw Security, go to <a href="job.html">Job Description</a> for
+       information of avaliable jobs or to <a href="apply.html">Apply</a> to apply for a avaliable job position.</p>
+       </section>
+      <!--Copyright link and email-->
+      <!--replaced the footer with a php include-->
+      <?php include 'footer.inc'; ?>
+</html>

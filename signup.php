410326d131b39a4c0a18772b05ec4056492abb74
Joshua <105934191@student.swin.edu.au>
2025-05-24 12:34:06 +1000

    added the styling to the pages

--- signup.php
+++ signup.php
@@ -49,6 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
+    <link rel="stylesheet" href="styles/styles.css">
 </head>
 <body>
     <?php include 'header.inc'; ?>
